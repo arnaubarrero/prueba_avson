478eeacf4be06c1a1b8ee781132dbe594fa23974
arnaubarrero <131618497+arnaubarrero@users.noreply.github.com>
2025-06-10 17:54:12 +0200

    Instalació del mapa

--- aplicacio/src/app/page.jsx
+++ aplicacio/src/app/page.jsx
@@ -1,10 +1,20 @@
-"use client"
+"use client";
 
-import Image from "next/image";
 import { useState, useEffect } from 'react';
-import Menu from "./components/menu/page"
+import dynamic from 'next/dynamic';
+import Menu from "./components/menu/page";
+
+const MapContainer = dynamic(
+  () => import('react-leaflet').then((mod) => mod.MapContainer),
+  { ssr: false }
+);
+const TileLayer = dynamic(
+  () => import('react-leaflet').then((mod) => mod.TileLayer),
+  { ssr: false }
+);
 
 export default function Home() {
+  const posicio = [37.985199, -1.125110];
   const [hora, setHora] = useState('');
 
   useEffect(() => {
@@ -16,14 +26,30 @@ export default function Home() {
 
     actualizarHora();
     const intervalo = setInterval(actualizarHora, 1000);
-
     return () => clearInterval(intervalo);
   }, []);
 
   return (
-    <div className="bg-white w-[100vw] h-[100vh] border border-white">
+    <div className="relative w-full h-screen">
+      {/* Full-screen map (client-side only) */}
+      <div className="w-full h-full fixed top-0 left-0 z-0">
+        <MapContainer
+          center={posicio}
+          zoom={12}
+          scrollWheelZoom={false}
+          className="w-full h-full"
+        >
+          <TileLayer
+            url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+            attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>'
+          />
+        </MapContainer>
+      </div>
 
-      < Menu />
+      {/* Your content (above the map) */}
+      <div className="relative z-10">
+        <Menu />
+      </div>
     </div>
   );
 }
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 ## Tecnologies utilitzades
 #### Mapa
 * leaftlet.js (OpenStreet Map)
-* * npm install leaflet react-leaflet
+* npm install leaflet react-leaflet
 
 #### Frontend
 * React (next.js)
