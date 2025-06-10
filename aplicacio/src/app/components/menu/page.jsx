7a6aa8ae35f39cb6ca27bb551620b94d3f85b03f
arnaubarrero <131618497+arnaubarrero@users.noreply.github.com>
2025-06-10 17:37:06 +0200

    Menu + logo svg + hora real (estetica basica)

--- aplicacio/src/app/components/menu/page.jsx
+++ aplicacio/src/app/components/menu/page.jsx
@@ -26,39 +26,34 @@ export default function Menu() {
     }, []);
 
     return (
-        <div className="flex items-center h-25">
+        <div className="flex items-center h-25 ">
             {/* Logo */}
-            <div className="w-1/3 pl-8">
-                <Image
-                    className="h-[60px] w-[60px]"
-                    priority
-                    src={logo}
-                    alt="El nostre logo"
-                />
+            <div className="w-1/3">
+                <Image className="h-[60px] w-[60px] ml-[25%]" priority src={logo} alt="El nostre logo" />
             </div>
 
             {/* Filtres */}
             <div className="w-1/3 flex flex-col items-center text-blue-800">
-                <h3 className="font-Outfit text-lg font-bold mb-1">Filtrar infraestructuras</h3>
-                <div className="flex gap-4">
-                    <div className="flex items-center gap-1">
+                <h3 className="font-Outfit text-2xl font-bold mb-1">Filtrar infraestructuras</h3>
+                <div className="flex flex-col gap-8 md:flex-row">
+                    <div className="flex items-center gap-3">
                         <input type="radio" name="infra" id="infra1" className="h-4 w-4" />
-                        <label htmlFor="infra1" className="text-sm">Infra 1</label>
+                        <label htmlFor="infra1" className="text-xl cursor-pointer">Infra 1</label>
                     </div>
-                    <div className="flex items-center gap-1">
+                    <div className="flex items-center gap-3">
                         <input type="radio" name="infra" id="infra2" className="h-4 w-4" />
-                        <label htmlFor="infra2" className="text-sm">Infra 2</label>
+                        <label htmlFor="infra2" className="text-xl cursor-pointer">Infra 2</label>
                     </div>
-                    <div className="flex items-center gap-1">
+                    <div className="flex items-center gap-3">
                         <input type="radio" name="infra" id="infra3" className="h-4 w-4" />
-                        <label htmlFor="infra3" className="text-sm">Infra 3</label>
+                        <label htmlFor="infra3" className="text-xl cursor-pointer">Infra 3</label>
                     </div>
                 </div>
             </div>
 
             {/* Hora e icona del menu */}
             <div className="w-1/3 flex justify-center items-center pr-6 gap-6">
-                <span className="text-gray-600 font-medium">{hora}</span>
+                <span className="text-2xl">{hora}</span>
                 <div className="cursor-pointer p-2 bg-[#E8F9FF] rounded-full">
                     <IconaMenu className="text-[#003B99]" />
                 </div>
