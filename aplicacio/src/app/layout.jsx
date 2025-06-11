85f6e02db5aa581a17de53725c7614e94d00489a
arnaubarrero <131618497+arnaubarrero@users.noreply.github.com>
2025-06-11 10:25:28 +0200

    font family outfit

--- aplicacio/src/app/layout.jsx
+++ aplicacio/src/app/layout.jsx
@@ -20,9 +20,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} >
+        <link href='https://fonts.googleapis.com/css?family=Outfit' rel='stylesheet'></link>
         {children}
       </body>
     </html>
